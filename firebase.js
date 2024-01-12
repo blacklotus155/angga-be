346a736cc942af39c283879eeb89e6fc3be371d9
blacklotus155 <147709019+blacklotus155@users.noreply.github.com>
2024-01-12 22:37:40 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getMessaging } = require('firebase-admin/messaging');
 
-const config = require('./angga-57c3d-172b90ec5ad2.json')
+const config = require('path').join(__dirname, 'angga-57c3d-172b90ec5ad2.json');
 
 const app = initializeApp({credential: cert(config)});
 
